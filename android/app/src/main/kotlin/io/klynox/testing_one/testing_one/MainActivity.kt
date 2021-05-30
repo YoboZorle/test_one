fb1af66c4eca7e99be2f1ea8683dad6d1a5d5198
Yobo Zorle <yobozorle@yahoo.com>
2021-05-30 16:50:59 +0100

    init latest dependency vesions

---
+++ android/app/src/main/kotlin/io/klynox/testing_one/testing_one/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.klynox.testing_one.testing_one
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:badges/badges.dart';
+import 'package:country_code_picker/country_code_picker.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            CountryCodePicker(
+              onChanged: print,
+              initialSelection: 'IT',
+              favorite: ['+39', 'FR'],
+              countryFilter: ['IT', 'FR'],
+              showFlagDialog: false,
+            ),
+            Badge(
+              badgeContent: Text('10'),
+              child: Icon(Icons.person, size: 30),
+            ),
+            SvgPicture.network(
+              'http://upload.wikimedia.org/wikipedia/commons/0/02/SVG_logo.svg',
+              placeholderBuilder: (BuildContext context) => Container(
+                  padding: const EdgeInsets.all(30.0),
+                  child: const CircularProgressIndicator(strokeWidth: 1)),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
