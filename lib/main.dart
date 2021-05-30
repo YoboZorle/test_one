5b807d0900e772c2632b542998a4220e52c45e17
Yobo Zorle <yobozorle@yahoo.com>
2021-05-30 16:55:33 +0100

    update flutterbadged dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:badges/badges.dart';
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_badged/flutter_badge.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -44,6 +45,11 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
+            ),
+            FlutterBadge(
+              icon: Icon(Icons.message),
+              borderRadius: 20.0,
+              itemCount: 3,
             ),
             CountryCodePicker(
               onChanged: print,
